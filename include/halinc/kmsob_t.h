29f48da31a8870fb2c92f4534bd1acb06b8602de
Ivanqi <512350225@qq.com>
2021-08-19 12:36:26 +0800

    【开发】增加代码

---
+++ include/halinc/kmsob_t.h
@@ -0,0 +1,14 @@
+/**********************************************************
+    内核内存空间对象分配释放头文件kmsob_t.h
+***********************************************************/
+
+#ifndef _KMSOB_T_H
+#define _KMSOB_T_H
+
+#define MSCLST_MAX (5)
+#define KOBLST_MAX (64)
+#define KUC_NEWFLG (1)
+#define KUC_DELFLG (2)
+#define KUC_DSYFLG (3)
+
+#endif
