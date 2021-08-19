5675a1e3ac90fc49754da78f4098a8badfde9a8b
Ivanqi <512350225@qq.com>
2021-08-19 23:13:50 +0800

    【开发】代码优化

--- script/lmkfbuild.c
+++ script/lmkfbuild.c
@@ -2,15 +2,14 @@
 #include "stdlib.h"
 #include "string.h"
 
-char* retn_ifilenm(int gc, char* argv[]) 
-{
+char* retn_ifilenm(int gc, char* argv[]) {
 	if (gc < 1 || NULL == argv) {
 		return NULL;
 	}
 
-	for(int i=0; i < gc; i++) {
-		if (strcmp("-i",argv[i]) == 0){
-			if (i<(gc-1)) {
+	for(int i = 0; i < gc; i++) {
+		if (strcmp("-i", argv[i]) == 0){
+			if (i < (gc - 1)) {
 				return argv[i + 1];
 			}
 		}
@@ -18,9 +17,8 @@ char* retn_ifilenm(int gc, char* argv[])
 	return NULL;
 }
 
-char* retn_ofilenm(int gc,char * argv[])
-{
-	if (gc<1||NULL==argv) {
+char* retn_ofilenm(int gc,char * argv[]) {
+	if (gc < 1 || NULL == argv) {
 		return NULL;
 	}
 
@@ -35,29 +33,34 @@ char* retn_ofilenm(int gc,char * argv[])
 	return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-	if(argc<4) {
+/**
+ * 文件转换
+ */
+int main(int argc, char *argv[]) {
+	if (argc < 4) {
 		printf("pram argc:%d\n", argc);
 		exit(1);
 	}
 
-	char* ifnm = retn_ifilenm(argc, argv);
-	char* ofnm = retn_ofilenm(argc, argv);
+	char* ifnm = retn_ifilenm(argc, argv);	// 输入的文件名
+	char* ofnm = retn_ofilenm(argc, argv);	// 输出的文件名
 
-	FILE* ifp = fopen(ifnm, "r");
-    FILE* ofp = fopen(ofnm, "w+");
+	FILE* ifp = fopen(ifnm, "r");			// 输入文件的句柄
+    FILE* ofp = fopen(ofnm, "w+");			// 输出文件的句柄
 
     if (NULL == ifp || NULL == ofp) {
     	printf("open fail:%s:%s\n", ifnm, ofnm);
     	exit(1);
     }
 
+	/**
+	 * 读出文件
+	 */
 	char fch = fgetc(ifp); 
 
 	while (fch != EOF) {
-    	if('['==fch) {
-    		fch='\t';
+    	if ('[' == fch) {
+    		fch = '\t';
     	}
 
         fputc(fch, ofp);
