a166749a0509edf678f5c519d6b6cd291ab224b0
Ivanqi <512350225@qq.com>
2021-10-23 21:59:06 +0800

    【开发】增加新头文件

---
+++ include/knlinc/krlsvethread_t.h
@@ -0,0 +1,7 @@
+/**********************************************************
+    内核服务头文件krlsvethread_t.h
+***********************************************************/
+#ifndef _KRLSVETHREAD_T_H
+#define _KRLSVETHREAD_T_H
+
+#endif // KRLSVETHREAD_T_H
