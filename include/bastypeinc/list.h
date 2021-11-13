39a51c1a341bd1e3ca29b4e396cbdd1df4be4120
Ivanqi <512350225@qq.com>
2021-11-13 22:53:06 +0800

    【开发】补充遗漏代码

--- include/bastypeinc/list.h
+++ include/bastypeinc/list.h
@@ -65,6 +65,20 @@ KLINE bool_t list_is_empty(const list_h_t *head) {
     return FALSE;
 }
 
+KLINE bool_t list_is_first(const list_h_t* list, const list_h_t* head) {
+    if (list->next == head) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+KLINE bool_t list_is_last(const list_h_t* list, const list_h_t* head) {
+    if (list->next == head) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
 KLINE bool_t list_is_empty_careful(const list_h_t *head) {
     list_h_t *next = head->next;
     if (next == head && next == head->prev) {
@@ -94,5 +108,15 @@ KLINE bool_t list_is_empty_careful(const list_h_t *head) {
 
 #define list_entry(ptr, type, member) ((type *)((char *)(ptr) - (unsigned long)(&((type *)0)->member)))
 
+/**
+ * list_t的指针减去它在整个结构体中的位置(&((type *)0)->member) (地址为0的type型数据中的member成员，就是member成员在type型数据中的偏移量），等于该结构体的开始
+ * The pointer of list_t minus its in the structure (&((type *)0)->member) (The member "member" in the data of type "type" at address 0, 
+ * equals to the offset of member "member" in type "type"), you will get the start address of that structure
+ */
+#define list_first_oneobj(head, o_type, o_member) list_entry((head)->next, o_type, o_member)
+
+#define list_next_entry(pos, type, member) list_entry((pos)->member.next, type, member)
+
+#define list_prev_entry(pos, type, member) list_entry((pos)->member.prev, type, member)
 
 #endif
