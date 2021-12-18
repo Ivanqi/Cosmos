c7b6ad99569abad262670d187c0921e85f539826
Ivanqi <512350225@qq.com>
2021-12-18 21:28:06 +0800

    【开发】增加遗漏文件

---
+++ kernel/krlservice.c
@@ -0,0 +1,18 @@
+/**********************************************************
+    内核服务文件krlservice.c
+***********************************************************/
+#include "cosmostypes.h"
+#include "cosmosmctrl.h"
+
+sysstus_t krlservice(uint_t inr, void* sframe)
+{
+    if (INR_MAX <= inr) {
+        return SYSSTUSERR;
+    }
+
+    if (NULL == osservicetab[inr]) {
+        return SYSSTUSERR;
+    }
+    
+    return osservicetab[inr](inr, (stkparame_t*)sframe);
+}
