8c6fc218b838b581da413b768cc0f2b155d24441
Ivanqi <512350225@qq.com>
2021-12-20 02:02:42 +0800

    【开发】编译代码优化

--- include/script/buildfile.h
+++ include/script/buildfile.h
@@ -19,13 +19,12 @@
 #define BUILD_MEMY_OBJS
 #define BUILD_FSYS_OBJS
 #define BUILD_DRIV_OBJS drvtick.o drvrfs.o drvuart.o
-#define BUILD_LIBS_OBJS
-// #define BUILD_LIBS_OBJS lapimm.o lapithread.o lapiopen.o lapiclose.o \
-//                         lapiread.o lapiwrite.o lapiioctrl.o lapilseek.o \
-//                         lapitime.o \
-//                         libmm.o libthread.o libopen.o libclose.o \
-//                         libread.o libwrite.o libioctrl.o liblseek.o \
-//                         libtime.o printf.o \
+#define BUILD_LIBS_OBJS lapimm.o lapithread.o lapiopen.o lapiclose.o \
+                        lapiread.o lapiwrite.o lapiioctrl.o lapilseek.o \
+                        lapitime.o \
+                        libmm.o libthread.o libopen.o libclose.o \
+                        libread.o libwrite.o libioctrl.o liblseek.o \
+                        libtime.o printf.o \
 
 #define BUILD_TASK_OBJS
 
