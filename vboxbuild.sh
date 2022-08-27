a28de6a22930892149998b68ca4c07a300726527
Ivanqi <512350225@qq.com>
2022-08-27 18:05:47 +0800

    【开发】增加新的文件

--- vboxbuild.sh
+++ vboxbuild.sh
@@ -3,4 +3,4 @@ sudo cp hd.img /Users/lvan/VirtualBox\ VMs
 cd /Users/lvan/VirtualBox\ VMs
 sudo rm -rf hd.vdi
 VBoxManage convertfromraw ./hd.img --format VDI ./hd.vdi
-VBoxManage internalcommands sethduuid  "./hd.vdi"  "5e0eb114-ebc0-47e0-b782-15be479c8a24"
+VBoxManage internalcommands sethduuid  "./hd.vdi"  "f602acba-4fec-4600-8fff-4fa7d6ac04ae"
